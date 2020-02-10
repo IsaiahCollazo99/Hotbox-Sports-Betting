9186b8185c9a377128a8a3f9e20039aab869eefd
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-10 13:21:52 -0500

    remove getPosts and populateNewsFeed() functions add in getFollowing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,53 +12,9 @@ const fetchData = async (url, cb) => {
     }
 } // End of fetchData() function
 
-const getPosts = async () => {
-    try {
-        let res = await axios.get(`http://localhost:3000/users/${userId}/posts`) 
-        let posts = res.data.post;
-        posts.filter(post => {
-            postsArr.push({posterId: post.id, body: post.body, timestamp: post.creation_date})
-        })
-    } catch(error) {
-        console.log(error)
-    }
-} // End of getPosts() function
+const getFollowing = (data) => {
 
-const populateNewsFeed = async () => {
-    let newsFeed = document.querySelector(".newsFeed")
-    let form = document.createElement("form")
-        
-    postsArr.forEach(post => {
-        // let newsFeed = document.querySelector(".newsFeed")
-        let p1 = document.createElement("p")
-        let p2 = document.createElement("p")
-        let section1 = document.createElement("section")
-        let section2 = document.createElement("section")
-        
-        let input = document.createElement("input")
-        let button = document.createElement("button")
-        
-        button.id = "commentButton"
-        form.id = "commentForm"
-        section1.id = post.id
-        section1.className = "nfPosts"
-        
-        p1.innerText = post.body
-        button.innerText = "Comment"
-        
-        newsFeed.appendChild(section1)
-        section1.appendChild(p1)
-        
-        section1.appendChild(form)
-        form.appendChild(input)
-        form.appendChild(button)
-        
-        section1.appendChild(section2)
-        
-    })
-
-    form.addEventListener("submit", addComment);
-} // End of populateNewsFeed() function
+} // End of getFollowing() function
 
 const addComment = (e) => {
     e.preventDefault()
@@ -68,7 +24,7 @@ const addComment = (e) => {
 } // End of addComment() function
 
 const setupPage = async () => {
-    await getPosts()
+    await fetchData("http://localhost:3000/users/" + userId + "/followings", getFollowing);
     await populateNewsFeed()
 } // End of setupPage() function
 
